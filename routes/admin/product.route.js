63c550469d70f5919a47ffbb74d4683ed078bbcf
Hoang Huy <you@example.com>
2024-08-23 09:57:49 +0700

    first commit

---
+++ routes/admin/product.route.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const route = express.Router();
+const multer = require("multer");
+const storageMulter = require("../../helpers/storageMulter");
+const upload = multer({storage: storageMulter()});
+
+const controller = require("../../controllers/admin/product.controller");
+const productValidate = require("../../validates/admin/productValidate");
+
+route.get("/",controller.index);
+route.patch("/change-status/:status/:id",controller.changeStatus);
+route.patch("/change-multi",controller.changeMulti);
+route.delete("/delete/:id",controller.deleteItem);
+route.get("/create",controller.createItem);
+route.post("/create",upload.single('thumbnail'),productValidate.createPost,controller.createItemPost);
+route.get("/edit/:id",controller.editItem);
+route.patch(
+    "/edit/:id",
+    upload.single('thumbnail'),
+    productValidate.createPost,
+    controller.editPatch
+)
+route.get("/detail/:id",controller.detailItem);
+module.exports = route;
