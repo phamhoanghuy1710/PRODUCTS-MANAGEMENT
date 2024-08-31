a1c22c93fa8d853444821ba107dad8426c64c370
Hoang Huy <you@example.com>
2024-08-31 21:51:12 +0700

    fe

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -170,7 +170,7 @@ if (sort){
         const [sortKey,sortValue] = e.target.value.split("-");
         url.searchParams.set("sortKey",sortKey);
         url.searchParams.set("sortValue",sortValue);
-        window.location.href = "facebook.com";
+        window.location.href = url.href;
     })
 
     // su kien cho sortClear
